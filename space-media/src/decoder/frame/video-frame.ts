17c79a2ac12f5098ea7d88d79547b00b688e82f0
wangxiaobo <wang_xiaobo@hzlh.com>
2023-03-22 17:58:07 +0800

    chore(base): init

--- space-media/src/decoder/frame/video-frame.ts
+++ space-media/src/decoder/frame/video-frame.ts
@@ -1,4 +1,4 @@
-import { Frame } from "./frame";
+import { Frame } from './frame';
 
 export type VideoFrameData = Uint8ClampedArray | Uint8Array;
 
@@ -6,12 +6,7 @@ export class VideoFrame extends Frame<VideoFrameData> {
   public width: number;
   public height: number;
 
-  public constructor(
-    width: number = 0, 
-    height: number = 0, 
-    pts: number = 0, 
-    data: Array<VideoFrameData> = new Array<VideoFrameData>
-  ) {
+  public constructor(width = 0, height = 0, pts = 0, data: Array<VideoFrameData> = new Array<VideoFrameData>()) {
     super(pts, data);
     this.width = width;
     this.height = height;
